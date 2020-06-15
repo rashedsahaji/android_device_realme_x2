bc4022aae3d6da6b65747cb37f2b81b9980ecbbe
rashedsahaji <asifrashed2@gmail.com>
2020-06-15 03:03:52 +0000

    Corvus: CorvusOS Bring Up!

--- du_x2.mk
+++ du_x2.mk
@@ -7,10 +7,17 @@
 $(call inherit-product, device/realme/x2/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+ IS_PHONE:= true
+
+#Gapps
+USE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := du_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
@@ -19,3 +26,4 @@ PRODUCT_MANUFACTURER := realme
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1587908485:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+EXTRA_FOD_ANIMATIONS := true
--- product_prop.mk
+++ product_prop.mk
@@ -7,3 +7,10 @@
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=16
+
+# USB Debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
